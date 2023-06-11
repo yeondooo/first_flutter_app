14a295809f228636dc7d449c4834f006ce19b546
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-06-11 22:18:53 +0900

    033. 공사중 해결 SingleChildScrollView

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -17,72 +17,74 @@ class _MainPageState extends State<MainPage> {
         title: Text('홈'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-            ),
-            SizedBox(height: 30),
-            Container(height: 30),
-            Text(
-              '숫자',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 40,
-              ),
-            ),
-            Text(
-              '$count',
-              style: TextStyle(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
                 color: Colors.red,
-                fontSize: 70,
+                width: 100,
+                height: 100,
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('ElevatedButton');
-              },
-              child: Text('ElevatedButton'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('TextButton'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('OutlinedButton'),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '글자',
-                border: OutlineInputBorder(),
-              ),
-              onChanged: (text) {
-                print(text);
-              },
-            ),
-            Image.network(
-              'https://blog.kakaocdn.net/dn/rIYaJ/btriQ6hL8tM/2k6nmswXNka1ykfhO7G3WK/img.png',
-              width: 100,
-              height: 100,
-              fit: BoxFit.cover,
-            ),
-            Container(
-              color: Colors.red,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset(
-                  'assets/pic.jpeg',
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
+              SizedBox(height: 100),
+              Container(height: 30),
+              Text(
+                '숫자',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
                 ),
               ),
-            ),
-          ],
+              Text(
+                '$count',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 70,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('ElevatedButton');
+                },
+                child: Text('ElevatedButton'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text('TextButton'),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('OutlinedButton'),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '글자',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (text) {
+                  print(text);
+                },
+              ),
+              Image.network(
+                'https://blog.kakaocdn.net/dn/rIYaJ/btriQ6hL8tM/2k6nmswXNka1ykfhO7G3WK/img.png',
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+              Container(
+                color: Colors.red,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/pic.jpeg',
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
