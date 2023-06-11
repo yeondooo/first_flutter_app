dfd1a0ebfc7361946b40d1f6feba6ac6893583a1
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-06-11 23:31:06 +0900

    047. iOS 스타일의 CupertinoApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_flutter_app/ios_style_screen.dart';
 import 'package:first_flutter_app/main_page.dart';
 import 'package:flutter/material.dart';
 
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainPage(),
+      home: const IosStyleScreen(),
     );
   }
 }
